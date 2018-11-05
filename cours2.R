048a2cf8ca504faef972466f7b2b6c19e2715479
TheFreshPenguin <31851170+TheFreshPenguin@users.noreply.github.com>
2018-11-05 16:01:47 +0100

    creating two different tabs for single users and all users

--- cours2.R
+++ cours2.R
@@ -14,19 +14,31 @@ don<-xts(x = data$Latitude, order.by = data$Time)
 ui <- dashboardPage(
   dashboardHeader(),
   dashboardSidebar(sidebarMenu(
+    menuItem("Single users", tabName = "singleUsers"),
+    menuItem("all users", tabName = "allUsers"),
     selectInput("var", 
     label = "Choose a type",
     choices = c("Friend", "Behaviour","Cheated","On time","Auto skipped","Skipped"),
     selected = "Friend") 
     )),
   dashboardBody(
+    tabItems(
+      tabItem(tabName = "allUsers", 
+              fluidRow(
+                box( 
+                    dygraph(don) %>%
+                    dyOptions(labelsUTC = TRUE, fillGraph=TRUE, fillAlpha=0.1, drawGrid = FALSE, colors="#D8AE5A") %>%
+                    dyRangeSelector() %>%
+                    dyCrosshair(direction = "vertical") %>%
+                    dyHighlight(highlightCircleSize = 5, highlightSeriesBackgroundAlpha = 0.2, hideOnMouseOut = FALSE)  %>%
+                    dyRoller(rollPeriod = 1)
+                  )
+              )),
+      tabItem(tabName = "singleUsers",
+              h2("single users "))
+    )
     
-    dygraph(don) %>%
-      dyOptions(labelsUTC = TRUE, fillGraph=TRUE, fillAlpha=0.1, drawGrid = FALSE, colors="#D8AE5A") %>%
-      dyRangeSelector() %>%
-      dyCrosshair(direction = "vertical") %>%
-      dyHighlight(highlightCircleSize = 5, highlightSeriesBackgroundAlpha = 0.2, hideOnMouseOut = FALSE)  %>%
-      dyRoller(rollPeriod = 1)
+    
     
   ))
 
